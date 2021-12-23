0874328d24ab6fcfc8496777896ba3f76cab8347
Arsh <arshgoyal2005@gmail.com>
2021-12-23 10:00:43 +0530

    added todo input

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import styled, { ThemeProvider } from 'styled-components';
 import { darkTheme, lightTheme, GlobalStyles } from './theme';
@@ -13,16 +13,25 @@ const Styled = styled.div`
 function App() {
 
   const [theme, setTheme] = useState('dark');
+  
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+    document.body.setAttribute('data-theme', theme);
+  })
+  
   const themeToggler = () => {
     theme === 'dark' ? setTheme('light') : setTheme('dark');
   }
 
   return (
     <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
+
     <GlobalStyles />
     <Styled className="container">
+
       <Header themeToggler={ themeToggler } theme={ theme } />
       <TodoInput />
+
     </Styled>
 
     </ThemeProvider>
--- src/theme.js
+++ src/theme.js
@@ -3,7 +3,7 @@ import { createGlobalStyle } from "styled-components"
 
 export const darkTheme = {
   bodyBackground: 'hsl(235, 21%, 11%)',
-  veryDarkBlue: 'hsl(235, 24%, 19%)',
+  todoBackground: 'hsl(235, 24%, 19%)',
   lightGrayishBlue: 'hsl(234, 39%, 85%)',
   lightGrayishBlueHover: 'hsl(236, 33%, 92%)',
   darkGrayishBlue: 'hsl(234, 11%, 52%)',
@@ -13,7 +13,7 @@ export const darkTheme = {
 
 export const lightTheme = {
   bodyBackground: 'hsl(0, 0%, 98%)',
-  veryLightGrayishBlue: 'hsl(236, 33%, 92%)',
+  todoBackground: 'hsl(236, 33%, 92%)',
   lightGrayishBlue: 'hsl(233, 11%, 84%)',
   darkGrayishBlue: 'hsl(236, 9%, 61%)',
   veryDarkGrayishBlue: 'hsl(235, 19%, 35%)',
@@ -25,4 +25,7 @@ export const GlobalStyles = createGlobalStyle`
 	body {
 		background-color: ${(props) => props.theme.bodyBackground};
 	}
+  .todo-input{
+    background: ${(props) => props.theme.todoBackground};
+  }
 `;
